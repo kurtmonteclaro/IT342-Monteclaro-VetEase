6513f6bc6e88c9a3469e45b328ff850a8a52cc8a
kurtmonteclaro <L13Y09W25@LabHCI.net>
2026-03-07 08:26:25 +0800

    feat: Added a backend structure

---
+++ backend/src/main/java/edu/cit/monteclaro/vetease/VeteaseApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.monteclaro.vetease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeteaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeteaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/cit/monteclaro/vetease/VeteaseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.monteclaro.vetease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeteaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
